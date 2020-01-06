83d20603b6df22f72ae79d873a5d573f2fa469bb
Xiang Tan <xtan@uccs.edu>
2020-01-05 19:18:31 -0700

    fixed this[init] is not a function error

--- js/app.js
+++ js/app.js
@@ -5,4 +5,4 @@ callback(response.data)}).catch(function(error){console.log(error.response)
 if(typeof error.response!=="undefined"){that.Alert({alert:!0,msg:error.response.statusText+" - Error Code: "+error.response.status,no:!1})}
 console.error(error)})}
 else{console.error("axios not loaded");this.myBlog=!1}},Alert:function(data){const that=this;that.err=data;window.setTimeout(function(){that.err={alert:!1}},3000)},cleanse:function(val){if(typeof val==="string"){val=JSON.parse(val)}
-return val},initblog:function(){const that=this;that.Ajax({data:!1,url:"/data.json"},function(result){if(typeof result.posts!=="undefined"){that.posts=result.posts}})},},mounted(){const init="init"+this.curPage;if(typeof init!=="undefined"){this[init]()}},template:"#temp-app"});
+return val},initblog:function(){const that=this;that.Ajax({data:!1,url:"/data.json"},function(result){if(typeof result.posts!=="undefined"){that.posts=result.posts}})},},mounted(){const init="init"+this.curPage;if(typeof this[init]==="function"){this[init]()}},template:"#temp-app"})
